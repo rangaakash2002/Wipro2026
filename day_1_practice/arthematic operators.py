12ecfcc1f1e2a5834c3776bfd6f2467388fdae0c
rangaakash2002 <rangaakash2002@gmail.com>
2026-01-15 12:49:53 +0530

    Add files via upload

---
+++ day_1_practice/arthematic operators.py
@@ -0,0 +1,9 @@
+a=int(input())
+b=int(input())
+print("addition",a+b)
+print("subtraction",a-b)
+print("multiplication",a*b)
+print("division",a/b)
+print("modulus",a%b)
+print("floor division",a//b)
+
---
+++ day_1_practice/drivinglicence.py
@@ -0,0 +1,5 @@
+x=int(input())
+if(x>18):
+    print("eligible to get driving licence")
+else:
+    print("not eligible to get driving licence")
