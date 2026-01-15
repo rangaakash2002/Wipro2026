7381168e9422a600f0be17367c2783c4be43b8da
rangaakash2002 <rangaakash2002@gmail.com>
2026-01-15 09:39:14 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,10 @@
+import math
+print(math.sqrt(5))
+print(math.log(10))
+print(math.gamma(20))
+a=10
+b=30
+if a==b:
+    print("true")
+else:
+    print("false")
---
+++ script1.py
@@ -0,0 +1,194 @@
+# print("hello")
+# x=input("enter your age")
+# if(int(x)>18):
+#     print("your are eligible to get licence")
+# else:
+#     print("you are not not eligible to get licence")
+
+
+# akash=input("enter your number")
+# if(int(akash)==10):
+#     print("yes it is mobile number")
+# else:
+#      print("give sufficient digits of mobile number")
+# a=10
+# b=45
+# print("addition",a+b)
+# print("subraction",a-b)
+# print("multiplication",a*b)
+# print("division",a/b)
+# print("floor division",a//b)
+# import math
+# print(math.sqrt(5))
+# print(math.log(10))
+# print(math.gamma(100))
+# a=500
+# b=1000
+# if(a==b):
+#     print("equal")
+# else:
+#     print("not equal")
+
+# a="akash ranga"
+# print(a[1])
+# print(a[-7])
+# print(a)text
+# print(a[6])
+# b="""surya"""
+# print(b[4])
+
+# text="narayana"
+# a="akash"
+# print(text[0:4])
+# print(text[:3])
+# print(text[4:])
+#
+# print(a+text)
+# print(len(a))
+# print(text.replace("narayana","ranga"))
+# print("akash"*3)
+# print(text.upper())
+#
+# print("text"not in"a")
+# s1="i am {0} ans i am {1} old".format("akash",22)
+# print(s1)
+# s2=a.split()
+# print(s2)
+
+# numbers=[20,30,60,80]
+# names=["ramu","somu","charan","kaja"]
+# mixed=[1,"rams",3.9,True]
+# print(numbers)
+# print(numbers[2])
+# numbers[2]=100
+# print(names[:2])
+# print(names)
+# for i in numbers:
+#     print(i)
+# if 30 in numbers:
+#     print("found in numbers")
+#
+# matrix=[[5,6,7],[2,3,4]]
+# print(matrix[1][2])
+# names.reverse()
+# print(names)
+# names.append("raju")
+# print(names)
+# names.remove("somu")
+# print(names)
+# mixed.insert(3,"sai")
+# print(mixed)
+# numbers.extend([55,88])
+# print(numbers)
+
+# t1=(10,22,23,24,25,27,22)
+# t2="akash","raju","somu","kalyan","somu"
+# print(t1[3])
+# print(t2[:2])
+# print(t2)
+# print(t1.count(22))
+# print(t2.count("somu"))
+# print(t1)
+# print(t2)
+# a=33
+# b=44
+# a,b=b,a
+# print(a,b)
+#
+# names="akash","jai","kiran"
+# m,n,o=names
+# print(m,n,o)
+#
+# a=1,2,3
+# b="ajay","somu","kiran"
+# print(a[1],b[1])
+
+# students={
+#     "names":"sai",
+#     "age":30,
+#     "course":"python",
+#
+# }
+# print(students)
+# students["age"]=25
+# students["grade"]="A"
+# print(students)
+# students.pop("age")
+# print(students)
+# students["mobile numbers"]=555555
+# print(students)
+# students.popitem()
+# print(students)
+
+
+# myset={22,333,4424,499,979,}
+# print(myset)
+# for i in myset:
+#     print(myset)
+# myset.add(323)
+# myset.add(4555)
+# myset.remove(499)
+# print(myset)
+#
+# a={1,4,5}
+# b={9,8,7}
+# print(a|b)
+# print(a&b)
+# print(5 in a)
+# print(88 in b)
+# b.pop()
+# # print(b)
+#
+#
+# num=6
+# if num%2==0:
+#     print("even")
+# else:
+#     print("odd")
+#
+# marks=40
+# if marks>90:
+#     print("grade A+")
+# elif marks>80:
+#     print("grade A")
+# elif marks>70:
+#     print("grade B+")
+# elif marks>60:
+#     print("grade C")
+# else:
+#     print("grad D")
+
+# j=1
+# while j<=8:
+#     print(j)
+#     j+=1
+#     if j==7:
+#         break
+
+# weekdays=5
+# match weekdays:
+#     case 1:
+#         print("monday")
+#     case 2:
+#         print("tuesday")
+#     case 3:
+#         print("wednesday")
+#     case 4:
+#         print("thursday")
+#     case 5:
+#         print("friday")
+#     case 6:
+#         print("saturday")
+
+# lambda functions
+
+# add=lambda a,b,c:a+b+c
+# print(add(4,7,5))
+# multi=lambda d,e,f:d*e*f
+# print(multi(7,8,9))
+
+# map+22
+# numbers=[8,9,7,6,5]
+# result=map(lambda x:x*2,numbers)
+# print(list(result
+
